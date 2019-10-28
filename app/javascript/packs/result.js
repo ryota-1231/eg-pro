85b7f4e8bf8b30a849c14cfa6bdbb176363c9c5c
ryota-1231 <rrrrr.1231@gmail.com>
2019-10-28 16:35:44 +0900

    カリキュラム結果実装
    ## What
    - カリキュラムのドリルの実装
    - ドリルが見本とあっているか判断し、正解、失敗を知らせる
    
    ## Why
    - ドリルをといて自動で正解かどうかの判断をすることでユーザーがどうすれば正解できるか考えられるようにする

---
+++ app/javascript/packs/result.js
@@ -0,0 +1,26 @@
+$(function(){
+  $('.result-btn').click(function(){
+    var dom_sample = document.getElementById('iframe')
+    var body_sample = dom_sample.contentWindow.document.querySelector('body')
+    var sample = $(body_sample).html()
+    var sm = sample.replace(/\s+/g, "")
+
+    var dom_result = document.getElementById('result_iframe')
+    var body_result = dom_result.contentWindow.document.querySelector('body')
+    var result = $(body_result).html()
+    var re = result.replace(/\s+/g, "")
+    var error = $('div').hasClass('ace_error')
+
+    if (sm===re && error==false) {
+      $('.js-modal2').fadeIn();
+    } else {
+      $('.js-modal3').fadeIn();
+    }
+  })
+
+  $('.js-modal-close').on('click',function(){
+    $('.js-modal2').fadeOut();
+    $('.js-modal3').fadeOut();
+    return false;
+  });
+})
