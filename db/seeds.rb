56ba9644f4aef1935c1ece31b73d5024e6bfd0ab
ryota-1231 <rrrrr.1231@gmail.com>
2019-11-08 08:01:27 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+require "csv"
+
+
+CSV.foreach('db/curriculum.csv', headers: true) do |row|
+    Curriculum.create(
+    # id: row['id'],
+    curriculums: row['sizetype'],
+    created_at: row['created_at'],
+    updated_at: row['updated_at'],
+    ancestry: row['ancestry'],
+    image: row['image']
+  )
+end
